3fa06f181ba09ed5328be4f25a851d4074c9cb95
DESKTOP-5S3HBRO\Patel <patelharshil737@gmail.com>
2017-06-01 09:42:03 +0530

    ListView added.

--- app/src/main/java/com/example/patel/quakereport/MainActivity.java
+++ app/src/main/java/com/example/patel/quakereport/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.patel.quakereport;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list);
     }
 }
